3f5057783f9ef9a4dffcd54dbe427d961a96dae0
Fabricio Stein <fstein@tndev.net>
2025-03-14 18:18:03 -0300

    0.0.1 => created library from framework

--- Package.swift
+++ Package.swift
@@ -5,20 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "NewFrameworkLibrary",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NewFrameworkLibrary",
-            targets: ["NewFrameworkLibrary"]),
+            targets: ["NewFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NewFrameworkLibrary"),
-        .testTarget(
-            name: "NewFrameworkLibraryTests",
-            dependencies: ["NewFrameworkLibrary"]
-        ),
+        // The destination of the framework you will obfuscate the code
+        .binaryTarget(name: "NewFramework", path: "./Sources/NewFramework.xcframework")
     ]
 )
