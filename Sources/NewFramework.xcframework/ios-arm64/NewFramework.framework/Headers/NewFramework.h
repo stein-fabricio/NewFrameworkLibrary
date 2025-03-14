0580a23737cf2d311efb11500992aa5c9b04bc62
Fabricio Stein <fstein@tndev.net>
2025-03-14 18:12:19 -0300

    Initial Commit

---
+++ Sources/NewFramework.xcframework/ios-arm64/NewFramework.framework/Headers/NewFramework.h
@@ -0,0 +1,18 @@
+//
+//  NewFramework.h
+//  NewFramework
+//
+//  Created by Fabricio Stein on 14/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NewFramework.
+FOUNDATION_EXPORT double NewFrameworkVersionNumber;
+
+//! Project version string for NewFramework.
+FOUNDATION_EXPORT const unsigned char NewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewFramework/PublicHeader.h>
+
+
